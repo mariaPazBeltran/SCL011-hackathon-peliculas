d37069bb776a102ebd2250f9bf1d7a58b7f8a897
María Beltrán <mariabeltra.98@gmail.com>
2019-10-01 16:08:31 -0300

    ok

--- src/main.js
+++ src/main.js
@@ -1,14 +1,1 @@
-let data = window.movies
-for (let i = 0; i < data.length; i++) {
-    console.log(data[i]);
-    let cards =document.createElement("div")
-    cards.className = "cardsMovies"
-    let buttonCards = document.createElement("button")
-    buttonCards.className = "buttonModal"
-    buttonCards.innerHTML ="VER"
-    
-    cards.innerHTML+= `<img class="posterMovie" src=${data[i].Poster}></img>` + `<h2>${data[i].Title}</h2>`
-    cards.appendChild(buttonCards)
-    document.getElementById("root").appendChild(cards).innerHTML
-    
-}
+
